c907ed5951b363bc303f98b3a1bb73fe33351740
CodeDotJS <rushforlinux@gmail.com>
2016-05-26 11:29:58 +0530

    url[rep]-assert[fix]

--- cli.js
+++ cli.js
@@ -3,129 +3,78 @@
 'use strict';
 
 const mkdirp = require('mkdirp');
-
 const https = require('follow-redirects').https;
-
 const boxen = require('boxen');
-
 const fs = require('fs');
-
 const colors = require('colors/safe');
 
 const argv = require('yargs')
-
 	.usage(colors.cyan.bold('\nUsage : $0 <command> [info] <option> [info]           '))
-
 	.command('u', colors.cyan.bold(' ❱ ') + ' instagram username ➨➤ High Resolution   ')
-
 	.command('m', colors.cyan.bold(' ❱ ') + ' insatgram username ➨➤ Medium Resolution ')
-
 	.command('w', colors.cyan.bold(' ❱ ') + ' insatgram username ➨➤ Low Resolution    ')
-
 	.command('l', colors.cyan.bold(' ❱ ') + ' full link to download image             ')
-
 	.command('v', colors.cyan.bold(' ❱ ') + ' full link to download video             ')
-
 	.demand(['n'])
-
 	.describe('n', colors.cyan.bold('❱') + '  save image or video as                  ')
-
-    .example('$0 -u [user-name] -n [image-name]')
-
-    .example('$0 -l [imageLink] -n [image-name]')
-
-    .example('$0 -v [videoLink] -n [video-name]')
-
+	.example('$0 -u [user-name] -n [image-name]')
+	.example('$0 -l [imageLink] -n [image-name]')
+	.example('$0 -v [videoLink] -n [video-name]')
 	.argv;
 
+const updateNotifier = require('update-notifier');
+const pkg = require('./package.json');
+updateNotifier({pkg}).notify();
+
 const options = {
-
 	hostname: 'www.instagram.com',
-
 	port: 443,
-
 	path: '/' + argv.u,
-
 	method: 'GET',
-
 	headers: {
-
 		'accept': 'text/html,application/json,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-
 		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36',
-
 		'Host': 'www.instagram.com',
-
 		'Connection': 'Keep-Alive',
-
 		'Accept-Language': 'en-GB,en-US;q=0.8,en;q=0.6'
-
 	}
-
 };
 
 const optionsMedium = {
-
 	hostname: 'www.instagram.com',
-
 	port: 443,
-
 	path: '/' + argv.m,
-
 	method: 'GET',
-
 	headers: {
-
 		'accept': 'text/html,application/json,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-
 		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36',
-
 		'Host': 'www.instagram.com',
-
 		'Connection': 'Keep-Alive',
-
 		'Accept-Language': 'en-GB,en-US;q=0.8,en;q=0.6'
-
 	}
-
 };
 
 const optionsSmall = {
-
 	hostname: 'www.instagram.com',
-
 	port: 443,
-
 	path: '/' + argv.w,
-
 	method: 'GET',
-
 	headers: {
-
 		'accept': 'text/html,application/json,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-
 		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36',
-
 		'Host': 'www.instagram.com',
-
 		'Connection': 'Keep-Alive',
-
 		'Accept-Language': 'en-GB,en-US;q=0.8,en;q=0.6'
-
 	}
-
 };
 
 const saveImage = './Instagram/';
-
 const removeSlash = saveImage.replace('./', '');
-
 const savedIn = removeSlash.replace('/', '');
 
 mkdirp(removeSlash, err => {
 	if (err) {
 		console.log(colors.red.bold('\n ❱ Directory Created      :    ✖\n'));
-
 		process.exit(1);
 	} else {
 		/* no need */
@@ -136,15 +85,11 @@ mkdirp(removeSlash, err => {
 function detectFullSize(urls) {
 	// checking the small size image.
 	const matchURL = urls.match(/150/g);
-
 	// available pixels
 	const badCase = null;
-
 	const checkURL = matchURL;
-
 	// storing pixel in array
 	const arrIndex = ['150'];
-
 	// checking if the pixles is available in the link
 	if (badCase === checkURL) {
 		return ['notHD'];
@@ -157,13 +102,9 @@ function detectFullSize(urls) {
 // for checking whether the remote image is available in medium resolution or not. [ 320x320 ]
 function detectMediumSize(urls) {
 	const mediumURL = urls.match(/320/g);
-
 	const caseMedium = null;
-
 	const checkURL = mediumURL;
-
 	const arrIndex = ['320'];
-
 	if (caseMedium === checkURL) {
 		return ['notHD'];
 	} else if (mediumURL[0] === arrIndex[0]) {
@@ -174,13 +115,10 @@ function detectMediumSize(urls) {
 // parsing images based on the resolution obtained.
 function redefineLink(contentLinks) {
 	const findPattern = contentLinks.match(/150/g);
-
 	// null because
 	const nullFire = null;
-
 	// finding the least resolution provided by instagram.
 	const gotPattern = ['150'];
-
 	// not all profile pictures are available in HD
 	if (nullFire === findPattern) {
 		// if pixel is missing
@@ -194,11 +132,8 @@ function redefineLink(contentLinks) {
 // for lowest resolution images [ 150x150 ]
 function detectSmallSize(urls) {
 	const smallURL = urls.match(/150/g);
-
 	const casesmall = null;
-
 	const checkURL = smallURL;
-
 	const arrIndex = ['150'];
 
 	if (casesmall === checkURL) {
@@ -211,9 +146,7 @@ function detectSmallSize(urls) {
 // function for parsing lowest resolution image's link [ missing 150x150 ]
 function parsedSmallImages(imgLink) {
 	const findPattern = imgLink.match(/150/g);
-
 	const nullFire = null;
-
 	const gotPattern = ['150'];
 
 	if (nullFire === findPattern) {
@@ -226,10 +159,8 @@ function parsedSmallImages(imgLink) {
 // checking if the given argument is an URL or not
 function checkURL(baseURL) {
 	const canValid = baseURL.match(/instagram.com/g);
-
 	// if output is null
 	const rareCase = null;
-
 	const mainValid = ['instagram.com'];
 
 	if (canValid === rareCase) {
@@ -256,7 +187,6 @@ checkInternet(isConnected => {
 	} else {
 		// stop the whole process if the network is unreachable
 		console.log(colors.red.bold('\n ❱ Internet Connection   :    ✖\n'));
-
 		process.exit(1);
 	}
 });
@@ -279,12 +209,10 @@ if (argv.u) {
 		} else {
 			// stopping the whole process if the username is invalid
 			console.log(colors.red.bold(' ❱ Valid Username        :    ✖\n'));
-
 			process.exit(1);
 		}
 
 		let store = '';
-
 		res.setEncoding = 'utf8';
 
 		res.on('data', d => {
@@ -293,20 +221,16 @@ if (argv.u) {
 
 		res.on('end', () => {
 			// regex to match the parsed image patterns.
-			const imagePattern = new RegExp(/profile_pic_url":"[a-zA-Z://\\-a-zA-Z.0-9\\-a-zA-Z.0-9]*/);
-
+			const imagePattern = new RegExp(/profile_pic_url": "[a-zA-Z://\\-a-zA-Z.0-9\\-a-zA-Z.0-9]*/);
 			const regMatches = store.match(imagePattern);
 
 			// [0] because we need only one link
 			if (regMatches && regMatches[0]) {
 				const imageLink = regMatches[0].replace('profile_pic_url":"', '');
-
 				// storing func's output in a variable.
 				const imageHD = detectFullSize(imageLink);
-
 				// stroing initial HD'ed image in array
 				const hdArray = ['HD'];
-
 				// storing initial notHD'ed image in array
 				const notHDArray = ['notHD'];
 
@@ -317,23 +241,18 @@ if (argv.u) {
 				} else if (notHDArray[0] === imageHD[0]) {
 					console.log(colors.red.bold('\n ❱ Image Resolution      :    ✖\n'));
 				}
-
 				// using previously made function
-				const remChars = redefineLink(imageLink);
+				const remChars = redefineLink(imageLink).replace('profile_pic_url": "', '');
 
 				// saving image
 				const imageFile = fs.createWriteStream(removeSlash + argv.n + '.jpg');
-
 				// downloading image
 				https.get(remChars, res => {
 					res.pipe(imageFile);
-
 					console.log(colors.cyan.bold(' ❱ Image Saved In        : '), ' ', colors.green.bold(savedIn), colors.cyan.bold('❱'), colors.green.bold(argv.n + '.jpg\n'));
 				}).on('error', err => {
 					console.log(err);
-
 					console.log('❱ Failed to Save the image');
-
 					process.exit(1);
 				});
 			}
@@ -358,12 +277,10 @@ if (argv.m) {
 			}, 1500);
 		} else {
 			console.log(colors.red.bold(' ❱ Valid Username        :    ✖\n'));
-
 			process.exit(1);
 		}
 
 		let store = '';
-
 		res.setEncoding = 'utf8';
 
 		res.on('data', d => {
@@ -371,17 +288,13 @@ if (argv.m) {
 		});
 
 		res.on('end', () => {
-			const imagePattern = new RegExp(/profile_pic_url_hd":"[a-zA-Z://\\-a-zA-Z.0-9\\-a-zA-Z.0-9]*/);
-
+			const imagePattern = new RegExp(/profile_pic_url_hd": "[a-zA-Z://\\-a-zA-Z.0-9\\-a-zA-Z.0-9]*/);
 			const regMatches = store.match(imagePattern);
 
 			if (regMatches && regMatches[0]) {
 				const imageLink = regMatches[0].replace('profile_pic_url_hd":"', '');
-
 				const imageHD = detectMediumSize(imageLink);
-
 				const hdArray = ['HD'];
-
 				const notHDArray = ['notHD'];
 
 				if (hdArray[0] === imageHD[0]) {
@@ -390,24 +303,19 @@ if (argv.m) {
 					console.log(colors.cyan.bold('\n ❱ Image Resolution      :    ✖\n'));
 				}
 
-				const remChars = redefineLink(imageLink);
-
+				const remChars = redefineLink(imageLink).replace('profile_pic_url_hd": "', '');
 				const imageFile = fs.createWriteStream(removeSlash + argv.n + '.jpg');
 
 				https.get(remChars, res => {
 					res.pipe(imageFile);
-
 					console.log(colors.cyan.bold(' ❱ Image Saved In        : '), ' ', colors.green.bold(savedIn), colors.cyan.bold('❱'), colors.green.bold(argv.n + '.jpg\n'));
 				}).on('error', err => {
 					console.log(err);
-
 					console.log('❱ Failed to Save the image');
-
 					process.exit(1);
 				});
 			} else {
 				console.log(colors.red.bold('\n ❱ Resolution Available  :    ✖\n'));
-
 				process.exit(1);
 			}
 		});
@@ -419,7 +327,6 @@ if (argv.w) {
 	const reqsmall = https.request(optionsSmall, res => {
 		if (res.statusCode === 200) {
 			console.log(colors.cyan.bold(' ❱ Valid Username        :    ✔'));
-
 			setTimeout(() => {
 				mkdirp(removeSlash, err => {
 					if (err) {
@@ -431,12 +338,10 @@ if (argv.w) {
 			}, 1500);
 		} else {
 			console.log(colors.red.bold(' ❱ Valid Username        :    ✖\n'));
-
 			process.exit(1);
 		}
 
 		let store = '';
-
 		res.setEncoding = 'utf8';
 
 		res.on('data', d => {
@@ -444,45 +349,35 @@ if (argv.w) {
 		});
 
 		res.on('end', () => {
-			const imagePattern = new RegExp(/profile_pic_url":"[a-zA-Z://\\-a-zA-Z.0-9\\-a-zA-Z.0-9]*/);
-
+			const imagePattern = new RegExp(/profile_pic_url": "[a-zA-Z://\\-a-zA-Z.0-9\\-a-zA-Z.0-9]*/);
 			const regMatches = store.match(imagePattern);
 
 			if (regMatches && regMatches[0]) {
 				const imageLink = regMatches[0].replace('profile_pic_url":"', '');
-
 				const imageHD = detectSmallSize(imageLink);
-
 				const hdArray = ['HD'];
-
 				const notHDArray = ['notHD'];
 
 				if (hdArray[0] === imageHD[0]) {
 					console.log(colors.cyan.bold('\n ❱ Image Resolution      :    ✔\n'));
 				} else if (notHDArray[0] === imageHD[0]) {
 					console.log(colors.red.bold('\n ❱ Image Resolution      :    ✖\n'));
-
 					process.exit(1);
 				}
 
-				const remChars = parsedSmallImages(imageLink);
-
+				const remChars = parsedSmallImages(imageLink).replace('profile_pic_url": "', '');
 				const imageFile = fs.createWriteStream(removeSlash + argv.n + '.jpg');
 
 				https.get(remChars, res => {
 					res.pipe(imageFile);
-
 					console.log(colors.cyan.bold(' ❱ Image Saved In        : '), ' ', colors.green.bold(savedIn), colors.cyan.bold('❱'), colors.green.bold(argv.n + '.jpg\n'));
 				}).on('error', err => {
 					console.log(err);
-
 					console.log('❱ Failed to Save the image');
-
 					process.exit(1);
 				});
 			} else {
 				console.log(colors.red.bold('\n ❱ Resolution Available  :    ✖\n'));
-
 				process.exit(1);
 			}
 		});
@@ -492,20 +387,16 @@ if (argv.w) {
 
 if (argv.l) {
 	const getNodeImage = argv.l;
-
 	const verifyLink = checkURL(argv.l);
-
 	const unvarLink = ['URL is not valid'];
 
 	if (verifyLink[0] === unvarLink[0]) {
 		console.log(colors.red.bold('\n ❱ Valid Link          :      ✖\n'));
-
 		process.exit(1);
 	}
 	const reqImages = https.request(getNodeImage, res => {
 		if (res.statusCode === 200) {
 			console.log(colors.cyan.bold(' ❱ Public Image          :    ✔\n'));
-
 			setTimeout(() => {
 				mkdirp(removeSlash, err => {
 					if (err) {
@@ -519,12 +410,10 @@ if (argv.l) {
 		} else {
 			// stopping the whole process if the username is invalid
 			console.log(colors.red.bold(' ❱ Public Image          :    ✖\n'));
-
 			process.exit(1);
 		}
 
 		let storePublic = '';
-
 		res.setEncoding = 'utf8';
 
 		res.on('data', d => {
@@ -532,26 +421,20 @@ if (argv.l) {
 		});
 
 		res.on('end', () => {
-			const imagePublicPattern = new RegExp(/display_src":"[a-zA-Z://\\-a-zA-Z.0-9\\-a-zA-Z.0-9]*/);
-
+			const imagePublicPattern = new RegExp(/display_src": "[a-zA-Z://\\-a-zA-Z.0-9\\-a-zA-Z.0-9]*/);
 			const regMatches = storePublic.match(imagePublicPattern);
 
 			if (regMatches && regMatches[0]) {
 				const imageLink = regMatches[0].replace('display_src":"', '');
-
-				const remChars = redefineLink(imageLink);
-
+				const remChars = redefineLink(imageLink).replace('display_src": "', '');
 				const imageFile = fs.createWriteStream(removeSlash + argv.n + '.jpg');
 
 				https.get(remChars, res => {
 					res.pipe(imageFile);
-
 					console.log(colors.cyan.bold(' ❱ Image Saved In        : '), ' ', colors.green.bold(savedIn), colors.cyan.bold('❱'), colors.green.bold(argv.n + '.jpg\n'));
 				}).on('error', err => {
 					console.log(err);
-
 					console.log('\n ❱ Failed to Save the image');
-
 					process.exit(1);
 				});
 			}
@@ -562,21 +445,17 @@ if (argv.l) {
 
 if (argv.v) {
 	const getNodeVideo = argv.v;
-
 	const verifyVideoLink = checkURL(argv.v);
-
 	const unvarVideoLink = ['URL is not valid'];
 
 	if (verifyVideoLink[0] === unvarVideoLink[0]) {
 		console.log(colors.red.bold('\n ❱ Valid Link          :      ✖\n'));
-
 		process.exit(1);
 	}
 
 	const reqVideo = https.request(getNodeVideo, res => {
 		if (res.statusCode === 200) {
 			console.log(colors.cyan.bold(' ❱ Public Video          :    ✔'));
-
 			setTimeout(() => {
 				mkdirp(removeSlash, err => {
 					if (err) {
@@ -590,39 +469,28 @@ if (argv.v) {
 		} else {
 			// stopping the whole process if the username is invalid
 			console.log(colors.red.bold(' ❱ Public Video          :    ✖ | Invalid Link\n'));
-
 			process.exit(1);
 		}
 
 		let storeVideo = '';
-
 		res.setEncoding = 'utf8';
-
 		res.on('data', d => {
 			storeVideo += d;
 		});
 
 		res.on('end', () => {
-			const videoPattern = new RegExp(/video_url":"[a-zA-Z://\\-a-zA-Z.0-9\\-a-zA-Z.0-9]*/);
-
+			const videoPattern = new RegExp(/video_url": "[a-zA-Z://\\-a-zA-Z.0-9\\-a-zA-Z.0-9]*/);
 			const regMatches = storeVideo.match(videoPattern);
-
 			if (regMatches && regMatches[0]) {
 				const imageLink = regMatches[0].replace('video_url":"', '');
-
-				const remChars = redefineLink(imageLink);
-
+				const remChars = redefineLink(imageLink).replace('video_url": "', '');
 				const videoFile = fs.createWriteStream(removeSlash + argv.n + '.mp4');
-
 				https.get(remChars, res => {
 					res.pipe(videoFile);
-
 					console.log(colors.cyan.bold('\n ❱ Video Saved In        : '), ' ', colors.green.bold(savedIn), colors.cyan.bold('❱'), colors.green.bold(argv.n + '.mp4\n'));
 				}).on('error', err => {
 					console.log(err);
-
 					console.log('\n ❱ Failed to Save the video');
-
 					process.exit(1);
 				});
 			}
