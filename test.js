e55ffc7986c2cad3f56e2dc3ba1962449c226659
Rishi Giri <rushforlinux@gmail.com>
2018-03-20 14:53:12 +0530

    add test for each case

--- test.js
+++ test.js
@@ -1,8 +1,52 @@
 import childProcess from 'child_process';
 import test from 'ava';
 
-test.cb('default', t => {
+test.cb('small', t => {
 	const cp = childProcess.spawn('./cli.js', ['-s', '9gag'], {stdio: 'inherit'});
+
+	cp.on('error', t.ifError);
+
+	cp.on('close', code => {
+		t.is(code, 0);
+		t.end();
+	});
+});
+
+test.cb('medium', t => {
+	const cp = childProcess.spawn('./cli.js', ['-m', '9gag'], {stdio: 'inherit'});
+
+	cp.on('error', t.ifError);
+
+	cp.on('close', code => {
+		t.is(code, 0);
+		t.end();
+	});
+});
+
+test.cb('large', t => {
+	const cp = childProcess.spawn('./cli.js', ['-f', '9gag'], {stdio: 'inherit'});
+
+	cp.on('error', t.ifError);
+
+	cp.on('close', code => {
+		t.is(code, 0);
+		t.end();
+	});
+});
+
+test.cb('image', t => {
+	const cp = childProcess.spawn('./cli.js', ['-l', 'https://www.instagram.com/p/BgiF0PsD1qx/?taken-by=iama_rishi'], {stdio: 'inherit'});
+
+	cp.on('error', t.ifError);
+
+	cp.on('close', code => {
+		t.is(code, 0);
+		t.end();
+	});
+});
+
+test.cb('video', t => {
+	const cp = childProcess.spawn('./cli.js', ['-v', 'https://www.instagram.com/p/BeTABLeFnxW/?taken-by=iama_rishi'], {stdio: 'inherit'});
 
 	cp.on('error', t.ifError);
 
