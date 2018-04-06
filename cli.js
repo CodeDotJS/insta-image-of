f101f64962f4459c4fd6def4ed327cffc024e721
Rishi Giri <rushforlinux@gmail.com>
2018-04-06 22:37:45 +0530

    add new feature
    download profile picture of any user in regular resolution - 640x640 px

--- cli.js
+++ cli.js
@@ -18,6 +18,7 @@ const pkg = require('./package.json');
 updateNotifier({pkg}).notify();
 
 const spinner = ora();
+const end = process.exit;
 const arg = process.argv[2];
 const user = process.argv[3];
 const pre = chalk.cyan.bold('›');
@@ -34,7 +35,7 @@ const checkConnection = () => {
 	dns.lookup('instagram.com', err => {
 		if (err) {
 			logUpdate(`\n${pos} ${dim('Please check your internet connection!')}\n`);
-			process.exit(1);
+			end(1);
 		}
 		logUpdate();
 		spinner.text = `Checking`;
@@ -45,7 +46,7 @@ const checkConnection = () => {
 const noUserArg = () => {
 	if (!user) {
 		logUpdate(`\n${pos} ${dim('<username/link> required\n')}`);
-		process.exit(1);
+		end(1);
 	}
 };
 
@@ -57,7 +58,7 @@ const returnBase = () => {
 const errorMessage = () => {
 	logUpdate(`\n${pos} ${user} ${dim('is not an Instagram user!')}\n`);
 	spinner.stop();
-	process.exit(1);
+	end(1);
 };
 
 const downloadMessage = () => {
@@ -67,25 +68,26 @@ const downloadMessage = () => {
 
 const privateError = () => {
 	logUpdate(`\n${pos} Maybe \n\n${pre} ${dim('Broken link')} or\n${pre} ${dim('Media shared by private profile')}\n`);
-	process.exit(1);
+	end(1);
 };
 
 fse.ensureDir(dir, err => {
 	if (err) {
-		process.exit(1);
+		end(1);
 	}
 });
 
-const argArray = ['-s', '--small', '-m', '--medium', '-f', '--full', '-l', '--link', '-v', '--video'];
+const argArray = ['-s', '--small', '-m', '--medium', '-f', '--full', '-r', '--regular', '-l', '--link', '-v', '--video'];
 
 if (!arg || arg === '-h' || arg === '--help' || argArray.indexOf(arg) === -1) {
 	console.log(`
  ${chalk.cyan('Usage')}	  : instavim [command] <username/link>
 
  ${chalk.cyan('Command')}  :       ${dim('<for profile picture>')}
-  -s, ${dim('--small')}     downlaod profile picture in small resolution
-  -m, ${dim('--medium')}    download profile picutre in medium resolution
-  -f, ${dim('--full')}      download profile picture in full resolution
+  -s, ${dim('--small')}     downlaod profile picture in small resolution   - ${dim('150  pixels')}
+  -m, ${dim('--medium')}    download profile picutre in medium resolution  - ${dim('320  pixels')}
+  -r, ${dim('--regular')}   download profile picture in regular resolution - ${dim('640  pixels')}
+  -f, ${dim('--full')}      download profile picture in full resolution    - ${dim('1080 pixels')}
 
  ${dim('Note : It works for the accounts which are both, public and private')}
 
@@ -98,7 +100,7 @@ if (!arg || arg === '-h' || arg === '--help' || argArray.indexOf(arg) === -1) {
 
  ${dim('Note : Works only if the link is associated with public media')}
   `);
-	process.exit(1);
+	end(1);
 }
 
 logUpdate();
@@ -114,7 +116,7 @@ const downloadMedia = (arg, ext, message) => {
 			saveImages.close(cb);
 			spinner.stop();
 			saveImages.on('error', () => {
-				process.exit(1);
+				end(1);
 			});
 		});
 	});
@@ -123,7 +125,7 @@ const downloadMedia = (arg, ext, message) => {
 const checkURL = link => {
 	if (isURL(link) === false) {
 		logUpdate(`\n${pos} ${dim('Please enter a valid url!')}\n`);
-		process.exit(1);
+		end(1);
 	}
 };
 
@@ -167,6 +169,22 @@ if (arg === '-s' || arg === '--small') {
 		if (err) {
 			errorMessage();
 		}
+	});
+} else if (arg === '-r' || arg === '--regular') {
+	returnBase();
+	got(url, {json: true}).then(res => {
+		const user = res.body.graphql.user.id;
+		const userProfile = `https://i.instagram.com/api/v1/users/${user}/info/`;
+		got(userProfile, {json: true}).then(res => {
+			downloadMessage();
+			const link = res.body.user.hd_profile_pic_versions[1].url;
+			downloadMedia(link, 'jpg', 'Image');
+		}).catch(err => {
+			if (err) {
+				logUpdate(`\n${pos} ${dim('The requested size is currently not available!')} \n`)
+				end(1);
+			}
+		});
 	});
 } else if (arg === '-l' || arg === '--link') {
 	returnBase();
